bc8e44468f0decc27b703644e0807755716ee89d
Charly <carlos@dango.digital>
2024-08-26 04:33:38 -0300

    c

--- backend/digitalOceanClient.js
+++ backend/digitalOceanClient.js
@@ -1,6 +1,5 @@
 const S3 = require("@aws-sdk/client-s3");
-const config = require("./config");
-
+const config = require("./config/config");
 const spacesEndpoint = "https://nyc3.digitaloceanspaces.com";
 
 const s3ClientPrevisiones = new S3({
